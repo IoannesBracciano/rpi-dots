07cf58a5cc265225152624e945798bedfb8c45d4
Ioannes Bracciano <john.bracciano@hotmail.gr>
2017-03-02 10:27:53 +0000

    New simple test suite

--- Dots_Test.py
+++ Dots_Test.py
@@ -8,149 +8,130 @@ import unittest
 import Dots
 
 
-# Tests the basic text formatting
-class TextFormattingTestCase(unittest.TestCase):
-  
-  def setUp(self):
-    self.buffer = Dots._Buffer().parse("\tDots\nBy Ioannes Bracciano")
-    self.first_line = self.buffer.line(0)
-    self.second_line = self.buffer.line(1)
+class SimpleTestCase(unittest.TestCase):
 
+  def test_parsing(self):
+    # Parse an empty string
+    Dots.buffer().parse("")
+    # Assert no lines are in the buffer
+    self.assertEqual(Dots.buffer().line_count(), 0,\
+        "Expected 0 lines in buffer, found {}".format(Dots.buffer().line_count()))
 
-  # Tests the number of _Lines in the _Buffer after the parsing of the text
-  def test_num_of_lines(self):
-    self.assertEqual(self.buffer.line_count(), 2,\
-        "Expected 2 _Lines in _Buffer, found {}".format(self.buffer.line_count()))
-
-
-  # Tests the number of _Cells in each _Line in the _Buffer
-  def test_num_of_cells(self):
-    self.assertEqual(self.first_line.cell_count(), 2,\
-        "Expected 2 _Cells in first _Line, found {}".format(self.first_line.cell_count()))
-
-    self.assertEqual(self.second_line.cell_count(), 1,\
-        "Expected 1 _Cell in second _Line, found {}".format(self.second_line.cell_count()))
-
-
-  # Tests the length of the formatted contents of each _Line in the _Buffer
-  def test_line_length(self):
-    self.assertEqual(len(str(self.first_line)), 16,\
-        "Expected 16 characters in first line, found {}".format(len(str(self.first_line))))
-    self.assertEqual(len(str(self.second_line)), 16,\
-        "Expected 16 characters in first line, found {}".format(len(str(self.second_line))))
-
-
-  # Tests tab stop positions and their effect in formatting the contents of the
-  # _Lines in the _Buffer
-  def test_line_tab_stops(self):
-
-    # Testing auto tab stops
-    tab_stops = self.first_line._tab_stops
-
-    self.assertEqual(len(tab_stops), 1,\
-        "Expected 1 tab stop in first _Line, found {}".format(len(tab_stops)))
-    self.assertEqual(tab_stops[0], 8,\
-        "Expected tab stop at position 8, found at {}".format(tab_stops[0]))
-    self.assertEqual(str(self.first_line), "        Dots    ",\
-        "Malformatted text in first line: {}".format(str(self.first_line)))
-
-    tab_stops = self.second_line._tab_stops
-
-    self.assertEqual(len(tab_stops), 0,\
-        "Expected 0 tab stops in second _Line, found {}".format(len(tab_stops)))
-    self.assertEqual(str(self.second_line), "By Ioannes Bracc",\
-        "Malformatted text in second line: {}".format(str(self.second_line)))
-
-    # Testing custom tab stops
-    self.first_line.set_tab_stops([6])
-    tab_stops = self.first_line._tab_stops
+    # Parse a simple string of two lines
+    Dots.buffer().parse("\tDots\nBy Ioannes Bracciano")
+    # Assert number of lines is 2
+    self.assertEqual(Dots.buffer().line_count(), 2,\
+        "Expected 2 lines in buffer, found: {}".format(Dots.buffer().line_count()))
+    # Assert first line has two cells
+    self.assertEqual(Dots.line(0).cell_count(), 2,\
+        "Expected 2 cells in first line, found {}".format(Dots.line(0).cell_count()))
+    # Assert second line has only one cell
+    self.assertEqual(Dots.line(1).cell_count(), 1,\
+        "Expected 1 cell in second line, found {}".format(Dots.line(1).cell_count()))
     
-    self.assertEqual(tab_stops[0], 6,\
-        " Expected tab stop at position 6, found at {}".format(tab_stops[0]))
-    self.assertEqual(str(self.first_line), "      Dots      ",\
-        "Malformatted text in first line: {}".format(str(self.second_line)))
+    # Append one more line to the buffer
+    Dots.buffer().append("One more line")
+    # Assert number of lines is now 3
+    self.assertEqual(Dots.buffer().line_count(), 3,\
+        "Expected 3 lines in buffer, found {}".format(Dots.buffer().line_count()))
+  
 
-    # Tests malformed tab stops
+  def test_formatting(self):
+    Dots.buffer().parse("0\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t15")
+    # Assert number of cells in line
+    self.assertEqual(Dots.line().cell_count(), 16,\
+        "Expected 16 cells in line, found: {}".format(Dots.line().cell_count()))
+    # Assert auto tab stops were calculated correctly
+    self.assertListEqual(Dots.line()._tab_stops, range(1,16),\
+        "Unexpected automatically set tab stops: {}".format(Dots.line()._tab_stops))
+    # Assert line formats the contents correctly
+    self.assertEqual(str(Dots.line()), "0123456789111111",\
+        "Contents of line malformed: {}".format(str(Dots.line())))
+
+    # Assert malformed tab stop arrays raise the correct exception
     with self.assertRaises(ValueError) as cm:
-      self.first_line.set_tab_stops([9,2])
+      Dots.line().set_tab_stops(range(15,0,-1))
     self.assertTrue("ascending" in cm.exception.message.lower())
 
     with self.assertRaises(ValueError) as cm:
-      self.first_line.set_tab_stops([])
+      Dots.line().set_tab_stops([])
     self.assertTrue("fewer" in cm.exception.message.lower())
 
-    
+    # Assert setting new tab stop positions affects the formatted contents of
+    # the line
+    Dots.line().set_tab_stops([1,2,3,4,5,6,7,8,9,11,13,15,17,19,21])
+    self.assertEqual(str(Dots.line()), "0123456789 10111",\
+        "Set tab stops, but contents did not format correctly: {}".format(str(Dots.line())))
 
-# Tests text scrolling
-class TextScrollingTestCase(unittest.TestCase):
 
-  def setUp(self):
-    self.buffer = Dots._Buffer().parse("\
-1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t15\t16\n\
-1\t2\t3\t4\t5\t6\t7\t8\t9\n\
-This\tis\ta\tthird\tline")
-    self.L1 = self.buffer.line(0)
-    self.L2 = self.buffer.line(1)
-    self.L3 = self.buffer.line(2)
-
-    self.L1.set_tab_stops([2,   4,  6,  8,  10, 12, 14, 16,\
-                           18,  20, 22, 24, 26, 28, 30, 32])
-
-  
-  # Tests the scrolling of the contents on the screen line by line
   def test_screen_scrolling(self):
-    self.assertEqual(self.buffer.line(), self.L1,\
-        "Expected current _Line in _Buffer to be '{}' before scrolling".format(str(self.L1)))
-    self.buffer.scroll_to(1)
-    self.assertEqual(self.buffer.line(), self.L2,\
-        "Expected current _Line in _Buffer to be '{}' after scrolling to it".format(str(self.L2)))
-    self.buffer.scroll_to(9)
-    self.assertEqual(self.buffer.line(), self.L3,\
-        "Expected current _Line in _Buffer to be '{}' after scrolling past it".format(str(self.L3)))
-    self.buffer.scroll_to(-1)
-    self.assertEqual(self.buffer.line(), self.L1,\
-        "Expected current _Line in _Buffer to be '{}' after scrolling before it".format(str(self.L1)))
+    # Parse five lines of text
+    Dots.buffer().parse("One\nTwo\nThree\nFour\nFive")
+    # Assert we are on the first line
+    self.assertEqual(Dots.line(), Dots.line(0),\
+        "Unexpected current line, should be '{}', found '{}'".format(str(Dots.line(0)), str(Dots.line())))
+    # Scroll down one line and assert we are on the second line
+    Dots.scroll().down().once()
+    self.assertEqual(Dots.line(), Dots.line(1),\
+        "Unexpected current line, should be '{}', found '{}'".format(str(Dots.line(1)), str(Dots.line())))
+    # Assert scrolling past the last line actually scrolls to it
+    Dots.scroll().down( by=4 ).once()
+    self.assertEqual(Dots.line(), Dots.line(4),\
+        "Unexpected current line, should be '{}', found '{}'".format(str(Dots.line(4)), str(Dots.line())))
+    # Assert scrolling before the first line actually scrolls to it
+    Dots.scroll().up( by=5 ).once()
+    self.assertEqual(Dots.line(), Dots.line(0),\
+        "Unexpected current line, should be '{}', found '{}'".format(str(Dots.line(0)), str(Dots.line())))
 
 
   def test_line_scrolling(self):
-    self.assertEqual(str(self.L1), "1 2 3 4 5 6 7 8 ",\
-        "Unexpected contents in first _Line before scrolling: {}".format(str(self.L1)))
-
-    self.L1.scroll_to(8) # Scroll to eighth cell
-    self.assertEqual(str(self.L1), "9 10111213141516",\
-        "Unexpected contents in first _Line after scrolling to the 8th cell: {}".format(str(self.L1)))
-    self.L1.scroll_to(17)
-    self.assertEqual(str(self.L1), "16",\
-        "Unexpected contents in first _Line after scrolling past its last cell: {}".format(str(self.L1)))
-    self.L1.scroll_to(-1)
-    self.assertEqual(str(self.L1), "1 2 3 4 5 6 7 8 ",\
-        "Unexpected contents in first _Line after scrolling before its first cell: {}".format(str(self.L1)))
-    
+    Dots.buffer().parse("0\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t15")
+    Dots.line().set_tab_stops(range(3,48,3))
+    # Assert we are on the first cell
+    self.assertEqual(Dots.line().cell(), Dots.line().cell(0),\
+        "Unexpected current cell, should be '{}', found '{}'".format(str(Dots.line().cell(0)), str(Dots.line().cell())))
+    # Scroll the line left and assert we are on the second cell
+    Dots.scroll(Dots.line()).left().once()
+    self.assertEqual(Dots.line().cell(), Dots.line().cell(1),\
+        "Unexpected current cell, should be '{}', found '{}'".format(str(Dots.line().cell(1)), str(Dots.line().cell())))
+    self.assertEqual(str(Dots.line()), "1  2  3  4  5  6",\
+        "Performed scrolling left, but contents of line did not scroll correctly: {}".format(str(Dots.line())))
+    # Assert scrolling past the last cell actually scrolls to it
+    Dots.scroll(Dots.line()).left( by=15 ).once()
+    self.assertEqual(Dots.line().cell(), Dots.line().cell(15),\
+        "Unexpected current cell, should be '{}', found '{}'".format(str(Dots.line().cell(15)), str(Dots.line().cell())))
+    self.assertEqual(str(Dots.line()), "15 ",\
+        "Tried scrolling past the last cell in line: {}".format(str(Dots.line())))
+    # Assert scrolling before the first cell actually scrolls to it
+    Dots.scroll(Dots.line()).right( by=16 ).once()
+    self.assertEqual(Dots.line().cell(), Dots.line().cell(0),\
+        "Unexpected current cell, should be '{}', found '{}'".format(str(Dots.line().cell(0)), str(Dots.line().cell())))
+    self.assertEqual(str(Dots.line()), "0  1  2  3  4  5",\
+        "Tried scrolling before the first cell in line: {}".format(str(Dots.line())))
 
 
   def test_cell_scrolling(self):
-    self.assertEqual(str(self.L3), "Thiis a  thiline",\
-        "Unexpected contents in third _Line before scrolling: {}".format(str(self.L3)))
-    self.L3.set_tab_stops([5,8,10,12])
-    self.assertEqual(str(self.L3), "This is a thline",\
-        "Unexpected contents in third _Line after setting tab stops: {}".format(str(self.L3)))
-    self.L3[3].scroll_to(1)
-    self.assertEqual(str(self.L3), "This is a hiline",\
-        "Unexpected contents in third _Line after scrolling 3rd cell by 1 character: {}".format(str(self.L3)))
-    self.L3[3].scroll_to(6)
-    self.assertEqual(str(self.L3), "This is a rdline",\
-        "Unexpected contents in third _Line after scrolling 3rd cell past the last character: {}".format(str(self.L3)))
-    self.L3[3].scroll_to(-1)
-    self.assertEqual(str(self.L3), "This is a thline",\
-        "Unexpected contents in third _Line after scrolling 3rd cell before the first character: {}".format(str(self.L3)))
-    
+    Dots.buffer().parse("abcdefghijklmnopqrstuvwxyz")
+    # Assert cell contents before scrolling
+    self.assertEqual(str(Dots.line().cell()), "abcdefghijklmnop",\
+        "Malformed cell contents: {}".format(str(Dots.line().cell())))
+    # Scroll cell to the left and assert cell contents scrolled
+    Dots.scroll(Dots.line().cell()).left().once()
+    self.assertEqual(str(Dots.line().cell()), "bcdefghijklmnopq",\
+        "Performed scrolling left, but contents of cell did not scroll correctly: {}".format(str(Dots.line().cell())))
+    # Assert scrolling past the last character in cell actually scrolls to it
+    Dots.scroll(Dots.line().cell()).left( by=25 ).once()
+    self.assertEqual(str(Dots.line().cell()), "klmnopqrstuvwxyz",\
+        "Tried scrolling past the last character in cell: {}".format(str(Dots.line().cell())))
+    # Assert scrolling before the first character in cell actually scrolls to it
+    Dots.scroll(Dots.line().cell()).right( by=26 ).once()
+    self.assertEqual(str(Dots.line().cell()), "abcdefghijklmnop",\
+        "Tried scrolling before the first character in cell: {}".format(str(Dots.line().cell())))
 
 
 
 if __name__ == "__main__":
-  suite = unittest.TestLoader().loadTestsFromTestCase(TextFormattingTestCase)
-  unittest.TextTestRunner(verbosity=2).run(suite)
-
-  suite = unittest.TestLoader().loadTestsFromTestCase(TextScrollingTestCase)
+  Dots.init()
+  suite = unittest.TestLoader().loadTestsFromTestCase(SimpleTestCase)
   unittest.TextTestRunner(verbosity=2).run(suite)
 
